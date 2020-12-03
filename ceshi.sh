0538df66bc73ede4a3cefa9bb4048fefb90aa635
liuqiuhui <LQH_linux@163.com>
2020-12-03 14:28:25 -0500

    v3

--- ceshi.sh
+++ ceshi.sh
@@ -2,7 +2,9 @@
 echo "ceshi" >> ceshi
 echo "test" >> test
 echo "111" >> 111
-if [ ! -f 222 ];then
-	echo "no file 222"
-	touch 222
+if [ ! -f 333 ];then
+	echo "no file 333"
+	touch 333
+else
+	echo "file 333 exist"
 fi
